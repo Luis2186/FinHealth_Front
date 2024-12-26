4b3c4fd47d9760b696c26ce66c315e815b04e151
Luis Lopez <l.lopezperdomo.e@gmail.com>
2024-12-26 18:31:29 -0300

    Layout, prueba de colores

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -4,7 +4,31 @@ import typography from '@tailwindcss/typography'
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+            colors: {
+                primary: {
+                  50:  '#fbf4ff',
+                  100: '#f5e6ff',
+                  200: '#ecd2ff',
+                  300: '#ddaeff',
+                  400: '#c97bff',
+                  500: '#b448ff',
+                  600: '#a225f8',
+                  700: '#8914d7',
+                  800: '#7616b3',
+
+                  900: '#61148f',
+                  950: '#43006b',
+                },
+                secondary: {
+                  // ...
+                },
+                tertiary: {
+                  // ...
+                },
+              },
+        },
 	},
 	plugins: [typography],
+    darkMode: 'class', 
 }
